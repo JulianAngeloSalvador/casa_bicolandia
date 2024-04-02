2bcba0588e4d91155c08fd47f2ec44e8c7b22d2c
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-04-02 23:23:12 +0800

    feat(server): implement async handling of db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { connectToMongoDB } from "./mongoose.js";
 
 const app = express();
 const port = 4000;
@@ -9,5 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Server connection indicator
-app.listen(port, () => console.log(`Server is running at port ${port}`));
+// Server and DB connection indicator
+async () => {
+  try {
+    await connectToMongoDB();
+    app.listen(port, () => console.log(`Server is running at port ${port}`));
+  } catch (err) {
+    console.error("Error: ", err);
+    process.exit(1);
+  }
+};
