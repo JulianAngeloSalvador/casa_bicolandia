b7dc4b72b70318c2b311007a49a71bad47d00c82
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-04-03 23:05:39 +0800

    feat(routes/root): implemented outlet component of react-router to handle children pages/components

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
-}
+    <>
+      <Outlet />
+    </>
+  );
+};
 
-export default Root
+export default Root;
