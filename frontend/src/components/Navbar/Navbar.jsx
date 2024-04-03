84865ce44f232a9e470cea7dde16845cd59c167b
fjpraxidio <fjpraxidio@gmail.com>
2024-04-03 21:44:57 +0800

    feat(src/components): implement navbar components

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.module.css'
+import logo from '../../../public/assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <img src={logo} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Product</li>
+        <li>Services</li>
+        <li>Blog</li>
+        <li>Contact Us</li>
+      </ul>
+      <ul>
+        <li>Sign up</li>
+        <li className='btn'>Login</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
