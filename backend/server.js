e625fe254ca0dcd53e5d6e259e20d4237a50e465
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-04-03 18:58:59 +0800

    feat: configure express server + add asynchronously handled mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,35 @@
 import express from "express";
 import cors from "cors";
-import { connectToMongoDB } from "./mongoose.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 4000;
+
+// For MongoDB
+const mongoURI = process.env.MONGO_DB;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(mongoURI);
+  } catch (error) {
+    console.error("Database Connection Error: ", error);
+  }
+}
 
 // Built-in middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Server and DB connection indicator
-async () => {
-  try {
-    await connectToMongoDB();
-    app.listen(port, () => console.log(`Server is running at port ${port}`));
-  } catch (err) {
-    console.error("Error: ", err);
-    process.exit(1);
-  }
-};
+//  Server connection indicator
+app.listen(port, () => console.log(`Server is running at port ${port}`));
+
+// DB connection indicator
+connectToMongoDB();
+mongoose.connection.on("error", (err) =>
+  console.error("Error encountered: ", err)
+);
+mongoose.connection.once("open", () =>
+  console.log("Database connection success")
+);
