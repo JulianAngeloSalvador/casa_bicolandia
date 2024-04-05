866404c32d054e31c760b69fdbf843320d2254d3
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-04-05 23:45:21 +0800

    feat(models/user): constructed user schema

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export const User = new mongoose.model("User", UserSchema);
