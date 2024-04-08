fd9c8778302cc6f1342123adbbbe5ccd18d0b043
fjpraxidio <fjpraxidio@gmail.com>
2024-04-08 21:43:32 +0800

    refactor(pages/app): added title and removed id

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,12 +1,17 @@
 import Hero from "../components/Hero/Hero";
 import Navbar from "../components/Navbar/Navbar";
+import Title from "../components/Title/Title";
 import "./app.css";
 
 export default function App() {
   return (
-    <section id="app">
+    <section>
       <Navbar />
       <Hero/>
+      <div>
+        <Title subTitle="Best Choices" title="Choose your room"/>
+
+      </div>
     </section>
   );
 }
